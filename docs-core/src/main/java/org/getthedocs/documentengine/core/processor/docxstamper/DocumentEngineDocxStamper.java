5c1965d3f4e88f680df28a5c1e8abd25a2e1e12f
videki <videki@users.noreply.github.com>
2025-06-23 12:42:03 +0200

    chore(task/GD-65): License header fix to exclude docx-stamper wrapper classes.

--- docs-core/src/main/java/org/getthedocs/documentengine/core/processor/docxstamper/DocumentEngineDocxStamper.java
+++ docs-core/src/main/java/org/getthedocs/documentengine/core/processor/docxstamper/DocumentEngineDocxStamper.java
@@ -1,25 +1,5 @@
 package org.getthedocs.documentengine.core.processor.docxstamper;
 
-/*-
- * #%L
- * docs-core
- * %%
- * Copyright (C) 2023 - 2025 Levente Ban
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import io.reflectoring.docxstamper.api.DocxStamperException;
 import io.reflectoring.docxstamper.api.commentprocessor.ICommentProcessor;
 import io.reflectoring.docxstamper.api.typeresolver.ITypeResolver;
@@ -37,13 +17,8 @@ import io.reflectoring.docxstamper.replace.typeresolver.DateResolver;
 import io.reflectoring.docxstamper.replace.typeresolver.FallbackResolver;
 import io.reflectoring.docxstamper.replace.typeresolver.image.Image;
 import io.reflectoring.docxstamper.replace.typeresolver.image.ImageResolver;
-import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.getthedocs.documentengine.core.processor.docxstamper.el.JsonExpressionResolver;
-import org.getthedocs.documentengine.core.provider.documentstructure.repository.filesystem.FileSystemDocumentStructureRepository;
-import org.getthedocs.documentengine.core.service.exception.TemplateServiceException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -51,11 +26,6 @@ import java.util.Date;
 import java.util.Map;
 
 public class DocumentEngineDocxStamper<T> {
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(DocumentEngineDocxStamper.class);
 
     private PlaceholderReplacer<T> placeholderReplacer;
 
--- docs-core/src/main/java/org/getthedocs/documentengine/core/processor/docxstamper/el/JsonExpressionResolver.java
+++ docs-core/src/main/java/org/getthedocs/documentengine/core/processor/docxstamper/el/JsonExpressionResolver.java
@@ -1,25 +1,5 @@
 package org.getthedocs.documentengine.core.processor.docxstamper.el;
 
-/*-
- * #%L
- * docs-core
- * %%
- * Copyright (C) 2023 - 2025 Levente Ban
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import io.reflectoring.docxstamper.api.EvaluationContextConfigurer;
 import io.reflectoring.docxstamper.el.ExpressionResolver;
 import io.reflectoring.docxstamper.el.ExpressionUtil;
