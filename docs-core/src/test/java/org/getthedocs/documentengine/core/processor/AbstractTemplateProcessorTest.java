a1830c8ac6ab0fe680954556e8663057c89c0f7a
videki <videki@users.noreply.github.com>
2025-06-23 12:12:13 +0200

    test(task/GD-60): TemplateProcessor test.

--- docs-core/src/test/java/org/getthedocs/documentengine/core/processor/AbstractTemplateProcessorTest.java
+++ docs-core/src/test/java/org/getthedocs/documentengine/core/processor/AbstractTemplateProcessorTest.java
@@ -9,9 +9,9 @@ package org.getthedocs.documentengine.core.processor;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,26 +20,25 @@ package org.getthedocs.documentengine.core.processor;
  * #L%
  */
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
-import java.io.InputStream;
-
-import org.getthedocs.documentengine.core.processor.AbstractTemplateProcessor;
 import org.getthedocs.documentengine.core.service.exception.TemplateServiceConfigurationException;
 import org.junit.Test;
 
-public class AbstractInputTemplateProcessorTest {
+import java.io.InputStream;
 
-  @Test
-  public void getTemplate() {
-    final String fileName = "unittests/docx/SimpleContract_v1_21-pojo.docx";
-    try {
-      InputStream i = AbstractTemplateProcessor.getTemplate(fileName);
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
-      assertNotNull(i);
-    } catch (TemplateServiceConfigurationException e) {
-      fail();
+public class AbstractTemplateProcessorTest {
+
+    @Test
+    public void getTemplate() {
+        final String fileName = "unittests/docx/SimpleContract_v1_21-pojo.docx";
+        try {
+            InputStream i = AbstractTemplateProcessor.getTemplate(fileName);
+
+            assertNotNull(i);
+        } catch (TemplateServiceConfigurationException e) {
+            fail();
+        }
     }
-  }
 }
